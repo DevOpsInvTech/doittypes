3ad237d85487a0440a1bd5a84972b3e058511591
Rob Cameron <rwcameron@gmail.com>
2015-05-14 00:40:07 -0700

    update schema

--- doit_host.go
+++ doit_host.go
@@ -8,7 +8,7 @@ import (
 type Host struct {
 	ID        int           `sql:"not null;unique;AUTO_INCREMENT" json:"id"`
 	Name      string        `sql:"unique" json:"name"`
-	Vars      []*HostVar    `gorm:"many2many:hostvars_vars;" json:"vars,omitempty"`
+	Vars      []*Var        `gorm:"many2many:hostvars_vars;" json:"vars,omitempty"`
 	Domain    *Domain       `json:"domain"`
 	DomainID  sql.NullInt64 `json:"-"`
 	CreatedAt time.Time     `json:"-"`
