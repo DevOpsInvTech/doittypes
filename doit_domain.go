4f0a2fee10642af62d4dc25be555537c069e8736
Rob Cameron <rwcameron@gmail.com>
2015-05-16 05:37:06 -0700

    add domain associations

--- doit_domain.go
+++ doit_domain.go
@@ -2,9 +2,13 @@ package doittypes
 
 import "time"
 
+//Domain a container for all related objects under the same namespace
 type Domain struct {
 	ID        int       `sql:"not null;unique;AUTO_INCREMENT" json:"id"`
 	Name      string    `sql:"unique" json:"name"`
+	Hosts     []Host    `gorm:"many2many:domain_hosts;" json:"hosts"`
+	Vars      []Var     `gorm:"many2many:domain_vars;"json:"vars"`
+	Groups    []Group   `gorm:"many2many:domain_groups;"json:"groups"`
 	CreatedAt time.Time `json:"-"`
 	UpdatedAt time.Time `json:"-"`
 }
