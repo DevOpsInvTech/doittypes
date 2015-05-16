5f55d2b5c2da80f181cba5a54d8e8706f9a96eb4
Rob Cameron <rwcameron@gmail.com>
2015-05-16 14:04:28 -0700

    update json for group

--- doit_group.go
+++ doit_group.go
@@ -8,7 +8,7 @@ import (
 type Group struct {
 	ID        int           `sql:"not null;unique;AUTO_INCREMENT" json:"id"`
 	Name      string        `json:"name"`
-	Domain    *Domain       `json:"domain"`
+	Domain    *Domain       `json:"-"`
 	DomainID  sql.NullInt64 `json:"-"`
 	Hosts     []*Host       `gorm:"many2many:group_hosts;" json:"hosts,omitempty"`
 	Vars      []*Var        `gorm:"many2many:group_vars;" json:"vars,omitempty"`
